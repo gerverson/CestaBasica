05342d50d5d9aef24b9bdf37791ced1f33700bc9
gerverson <gerverson00@gmail.com>
2019-10-30 09:05:38 -0300

    up

--- cestabasica/cesta/views.py
+++ cestabasica/cesta/views.py
@@ -65,8 +65,8 @@ def home(request):
         data['mes'] = 'Dez'
     data['ano'] = ano
 
-    # return HttpResponse(txw)
-    return render(request, 'home.html', data)
+    return HttpResponse(tx[3])
+    # return render(request, 'home.html', data)
 
 
 def estatistica(request, id):
